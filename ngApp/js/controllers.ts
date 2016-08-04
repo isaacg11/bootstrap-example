f5cb5be356e0719596f88784febe176247d72dd9
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-08-04 16:36:51 -0700

    added ui-modals

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,10 +1,36 @@
 namespace app.Controllers {
   export class HomeController {
 
-    constructor() {
+    showModal(animalName:string) {
+      this.$uibModal.open({
+        templateUrl: '/templates/dialog.html',
+        controller: 'DialogController',
+        controllerAs: 'vm',
+        resolve: {
+          dataFromHomeController: () => animalName
+        },
+        size: 'sm'
+      });
+    }
+
+    constructor(private $uibModal: angular.ui.bootstrap.IModalService) {
 
     }
   }
 
   angular.module('app').controller('HomeController', HomeController);
+
+  class DialogController {
+    ok() {
+      this.$uibModalInstance.close();
+    }
+
+    constructor(
+      public dataFromHomeController,
+      private $uibModalInstance: angular.ui.bootstrap.IModalServiceInstance) {
+
+    }
+  }
+  angular.module("app").controller("DialogController", DialogController);
+
 }
